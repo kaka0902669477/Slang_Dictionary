335671ee678ccc15d02b97149832e4cc57efc1aa
kaka0902669477 <64117392+kaka0902669477@users.noreply.github.com>
2021-12-20 21:55:49 +0700

    AddList

--- src/fitus/Main.java
+++ src/fitus/Main.java
@@ -2,7 +2,11 @@ package fitus;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+        // TODO Auto-generated method stub
+        new MenuFrame();
+        // String current = new java.io.File(".").getCanonicalPath();
+        // System.out.println("Current dir:" + current);
     }
+
 }
