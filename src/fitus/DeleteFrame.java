d4804c075b78e1cb9f8630128038fd4fd70dce10
kaka0902669477 <64117392+kaka0902669477@users.noreply.github.com>
2021-12-26 22:00:44 +0700

    fixed add

--- src/fitus/DeleteFrame.java
+++ src/fitus/DeleteFrame.java
@@ -138,4 +138,12 @@ public class DeleteFrame extends JFrame implements ActionListener, ListSelection
 
         }
     }
+
+    public void addRow() {
+        int size = data.length;
+        for (int i = 0; i < size; i++) {
+            String ss[] = data[i];
+            model.addRow(ss);
+        }
+    }
 }
